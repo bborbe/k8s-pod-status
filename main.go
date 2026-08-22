184be462e34a260c2365bc330b42ae3fa1a1349a
Benjamin Borbe <benjamin.borbe@gmail.com>
2026-08-22 20:29:10 +0200

    emit version label on build_info via shared metrics lib

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 
 	libhttp "github.com/bborbe/http"
 	libk8s "github.com/bborbe/k8s"
+	libmetrics "github.com/bborbe/metrics"
 	"github.com/bborbe/run"
 	libsentry "github.com/bborbe/sentry"
 	"github.com/bborbe/service"
@@ -20,7 +21,6 @@ import (
 
 	"github.com/bborbe/k8s-pod-status/pkg/factory"
 	libfactory "github.com/bborbe/k8s-pod-status/pkg/libfactory"
-	libmetrics "github.com/bborbe/k8s-pod-status/pkg/libmetrics"
 )
 
 func main() {
@@ -29,17 +29,18 @@ func main() {
 }
 
 type application struct {
-	SentryDSN      string            `required:"true"  arg:"sentry-dsn"       env:"SENTRY_DSN"       usage:"SentryDSN"                 display:"length"`
-	SentryProxy    string            `required:"false" arg:"sentry-proxy"     env:"SENTRY_PROXY"     usage:"Sentry Proxy"`
-	Listen         string            `required:"true"  arg:"listen"           env:"LISTEN"           usage:"address to listen to"`
-	Kubeconfig     string            `required:"false" arg:"kubeconfig"       env:"KUBECONFIG"       usage:"Path to k8s config"`
-	Namespace      libk8s.Namespace  `required:"true"  arg:"namespace"        env:"NAMESPACE"        usage:"Kubernetes namespace"`
-	BuildGitCommit string            `required:"false" arg:"build-git-commit" env:"BUILD_GIT_COMMIT" usage:"Build Git commit hash"                      default:"none"`
-	BuildDate      *libtime.DateTime `required:"false" arg:"build-date"       env:"BUILD_DATE"       usage:"Build timestamp (RFC3339)"`
+	SentryDSN       string            `required:"true"  arg:"sentry-dsn"        env:"SENTRY_DSN"        usage:"SentryDSN"                                                display:"length"`
+	SentryProxy     string            `required:"false" arg:"sentry-proxy"      env:"SENTRY_PROXY"      usage:"Sentry Proxy"`
+	Listen          string            `required:"true"  arg:"listen"            env:"LISTEN"            usage:"address to listen to"`
+	Kubeconfig      string            `required:"false" arg:"kubeconfig"        env:"KUBECONFIG"        usage:"Path to k8s config"`
+	Namespace       libk8s.Namespace  `required:"true"  arg:"namespace"         env:"NAMESPACE"         usage:"Kubernetes namespace"`
+	BuildGitVersion string            `required:"false" arg:"build-git-version" env:"BUILD_GIT_VERSION" usage:"Build Git version (git describe --tags --always --dirty)"                  default:"dev"`
+	BuildGitCommit  string            `required:"false" arg:"build-git-commit"  env:"BUILD_GIT_COMMIT"  usage:"Build Git commit hash"                                                     default:"none"`
+	BuildDate       *libtime.DateTime `required:"false" arg:"build-date"        env:"BUILD_DATE"        usage:"Build timestamp (RFC3339)"`
 }
 
 func (a *application) Run(ctx context.Context, sentryClient libsentry.Client) error {
-	libmetrics.NewBuildInfoMetrics().SetBuildInfo(a.BuildDate)
+	libmetrics.NewBuildInfoMetrics().SetBuildInfo(a.BuildGitVersion, a.BuildGitCommit, a.BuildDate)
 
 	return service.Run(
 		ctx,
